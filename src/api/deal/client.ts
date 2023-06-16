9166679c92e05a3cc5f96a1f02e0318b9d36b5ab
Denis Belyaev <battler73@yandex.ru>
2023-06-16 12:57:16 +0300

    feat(deals): add typings for Deal Import API

--- src/api/deal/client.ts
+++ src/api/deal/client.ts
@@ -1,18 +1,18 @@
 import { Endpoint } from '../../core/endpoint'
-import type { ApiResponse } from '../../typings/utility'
+import type { ImportApiResponse } from '../../typings/utility'
 import type { RequestAddDeal, RequestUpdateDealStatus, ResponseDeal } from './types'
 
 export class DealApi extends Endpoint {
-  addDeal(deal: RequestAddDeal): Promise<ApiResponse<ResponseDeal>> {
-    return this.rest.post({
+  addDeal(deal: RequestAddDeal): Promise<ImportApiResponse<ResponseDeal>> {
+    return this.rest.post<ImportApiResponse<ResponseDeal>>({
       url: '/pl/api/deals',
       action: 'add',
       params: deal,
     })
   }
 
-  updateStatus(deal: RequestUpdateDealStatus): Promise<ApiResponse<ResponseDeal>> {
-    return this.rest.post({
+  updateStatus(deal: RequestUpdateDealStatus): Promise<ImportApiResponse<ResponseDeal>> {
+    return this.rest.post<ImportApiResponse<ResponseDeal>>({
       url: '/pl/api/deals',
       action: 'add',
       params: deal,
