014967d1e93e43a946e7a3a89eb50c41d05ef0a4
Denis Belyaev <battler73@yandex.ru>
2023-06-16 12:38:34 +0300

    feat: split import/export & add AtLeastOne type

--- src/typings/utility.ts
+++ src/typings/utility.ts
@@ -6,8 +6,20 @@ export type JSONValue =
   | { [x: string]: JSONValue }
   | Array<JSONValue>
 
-export type ApiResponse<T> = {
+export type RequireAtLeastOne<T> = {
+  [K in keyof T]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<keyof T, K>>>
+}[keyof T]
+
+export type ImportApiResponse<T> = {
   success: boolean
   action: string
   result: T
+}
+
+export type ExportApiResponse<T> = {
+  success: boolean
+  info: T
+  error_message: string
+  error: boolean
+  error_code?: number
 }
