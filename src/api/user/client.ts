cd3efebba4043a6136930f5b1d437fdf7ac05a7a
Denis Belyaev <battler73@yandex.ru>
2023-06-16 16:07:15 +0300

    feat(user): Добавлены описания методов

--- src/api/user/client.ts
+++ src/api/user/client.ts
@@ -3,6 +3,11 @@ import type { ImportApiResponse } from '../../typings/utility'
 import type { RequestAddUser, RequestUpdateUserGroups, ResponseUser } from './types'
 
 export class UserApi extends Endpoint {
+  /**
+   * Создание пользователя в GetCourse
+   * @param {RequestAddUser} user Информация о пользователе
+   * @returns {ImportApiResponse<ResponseUser>} Созданный пользователь
+   */
   addUser(user: RequestAddUser): Promise<ImportApiResponse<ResponseUser>> {
     return this.rest.post<ImportApiResponse<ResponseUser>>({
       url: '/pl/api/users',
@@ -11,6 +16,15 @@ export class UserApi extends Endpoint {
     })
   }
 
+  /**
+   * Управление группами пользователя в GetCourse
+   *
+   * В результате выполнения запроса пользователь будет состоять во всех группах, указанных в списке group_name.
+   *
+   * Если ранее пользователь состоял в каких-либо группах, не указанных в group_name — он будет удален из этих групп.
+   * @param {RequestAddUser} user ID пользователя и список групп
+   * @returns {ImportApiResponse<ResponseUser>} Обновленный пользователь
+   */
   updateGroups(user: RequestUpdateUserGroups): Promise<ImportApiResponse<ResponseUser>> {
     return this.rest.post<ImportApiResponse<ResponseUser>>({
       url: '/pl/api/users',
