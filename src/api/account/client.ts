88cec13af97cc57d16c072556c320c9b5ba39a7f
Denis Belyaev <battler73@yandex.ru>
2023-06-16 16:06:55 +0300

    feat(account): Полностью доработан Export API

--- src/api/account/client.ts
+++ src/api/account/client.ts
@@ -1,51 +1,79 @@
 import { Endpoint } from '../../core/endpoint'
-import type { FilterLike } from '../../helpers/filter'
 import { query } from '../../helpers/query'
+import type { ExportApiResponse, RequireAtLeastOne } from './../../typings/utility'
+import type { ExportInfo, ExportedData, FieldInfo, RequestExportDeals, RequestExportGroups, RequestExportGroupsUsers, RequestExportPayments, RequestExportUsers } from './types'
 
 export class AccountApi extends Endpoint {
-  exportUsers(params?: { filter?: FilterLike<['status'], ['created_at']> }) {
-    return this.rest.post({
+  /** Выгрузка пользователей из GetCourse */
+  exportUsers(params: RequireAtLeastOne<RequestExportUsers>): Promise<ExportApiResponse<ExportInfo>> {
+    return this.rest.post<ExportApiResponse<ExportInfo>>({
       url: '/pl/api/account/users',
-      //   action: "get",
       query: query(params),
     })
   }
 
-  exportGroups(params?: { filter?: FilterLike<['status'], ['created_at', 'added_at']> }) {
-    return this.rest.post({
+  /** Выгрузка групп из GetCourse
+   * @param {RequireAtLeastOne<RequestExportGroups>} params Фильтр
+   * @returns {ExportApiResponse<ExportInfo>} ID задачи на экспорт
+  */
+  exportGroups(params: RequireAtLeastOne<RequestExportGroups>): Promise<ExportApiResponse<ExportInfo>> {
+    return this.rest.post<ExportApiResponse<ExportInfo>>({
       url: '/pl/api/account/users',
-      // action: "get",
       query: query(params),
     })
   }
 
-  exportGroupUsers(group_id: number, params?: { filter?: FilterLike<['status'], ['created_at', 'added_at']> }) {
-    return this.rest.post({
+  /** Выгрузка участников групп из GetCourse
+   * @param {number} group_id ID группы
+   * @param {RequestExportGroupsUsers=} params Фильтр
+   * @returns {ExportApiResponse<ExportInfo>} ID задачи на экспорт
+   */
+  exportGroupUsers(group_id: number, params?: RequestExportGroupsUsers): Promise<ExportApiResponse<ExportInfo>> {
+    return this.rest.post<ExportApiResponse<ExportInfo>>({
       url: `/pl/api/account/groups/${group_id}/users`,
-      // action: "get",
       query: query(params),
     })
   }
 
-  exportPayments(params?: { filter?: FilterLike<['status'], ['created_at', 'status_changed_at']> }) {
-    return this.rest.post({
+  /** Выгрузка платежей из GetCourse
+   * @param {RequireAtLeastOne<RequestExportPayments>} params Фильтр
+   * @returns {ExportApiResponse<ExportInfo>} ID задачи на экспорт
+   */
+  exportPayments(params: RequireAtLeastOne<RequestExportPayments>): Promise<ExportApiResponse<ExportInfo>> {
+    return this.rest.post<ExportApiResponse<ExportInfo>>({
       url: '/pl/api/account/payments',
-      // action: "get",
       query: query(params),
     })
   }
 
-  getFields() {
-    return this.rest.post({
+  /** Выгрузка заказов из GetCourse
+   * @param {RequireAtLeastOne<RequestExportDeals>} params Фильтр
+   * @returns {ExportApiResponse<ExportInfo>} ID задачи на экспорт
+   */
+  exportDeals(params: RequireAtLeastOne<RequestExportDeals>): Promise<ExportApiResponse<ExportInfo>> {
+    return this.rest.post<ExportApiResponse<ExportInfo>>({
+      url: '/pl/api/account/deals',
+      query: query(params),
+    })
+  }
+
+  /** Выгрузка полей из GetCourse
+   * @returns {ExportApiResponse<FieldInfo[]>} Список полей
+   */
+  getFields(): Promise<ExportApiResponse<FieldInfo[]>> {
+    return this.rest.post<ExportApiResponse<FieldInfo[]>>({
       url: '/pl/api/account/fields',
       action: 'get',
     })
   }
 
-  getExportResult(export_id: number) {
-    return this.rest.post({
+  /** Получение сформированной выгрузки из GetCourse
+   * @param {number} export_id ID задачи на экспорт
+   * @returns {ExportApiResponse<ExportedData>} Выгруженные данные
+   */
+  getExportResult(export_id: number): Promise<ExportApiResponse<ExportedData>> {
+    return this.rest.post<ExportApiResponse<ExportedData>>({
       url: `/pl/api/account/exports/${export_id}`,
-      // action: "get",
     })
   }
 }
---
+++ src/api/account/types.ts
@@ -0,0 +1,136 @@
+import type {
+  DealStatus,
+  PaymentStatus,
+  UserStatus,
+} from '../../typings/entities'
+
+/** Фильтры экспорта пользователей */
+export type RequestExportUsers = {
+  /** Статус пользователя */
+  status?: UserStatus
+  /** Дата создания пользователей */
+  created_at?: {
+    /** @example 2022-11-01 */
+    from: string
+    /** @example 2022-11-01 */
+    to: string
+  }
+}
+
+/** Фильтры экспорта пользователей */
+export type RequestExportGroups = {
+  /** Статус пользователя */
+  status?: UserStatus
+  /** Дата создания пользователей */
+  created_at?: {
+    /** @example 2022-11-01 */
+    from: string
+    /** @example 2022-11-01 */
+    to: string
+  }
+}
+
+/** Фильтры экспорта пользователей */
+export type RequestExportGroupsUsers = {
+  /** Статус пользователя */
+  status?: UserStatus
+  /** Дата создания пользователей */
+  created_at?: {
+    /** @example 2022-11-01 */
+    from: string
+    /** @example 2022-11-01 */
+    to: string
+  }
+  /** Дата добавления в группу */
+  added_at?: {
+    /** @example 2022-11-01 */
+    from: string
+    /** @example 2022-11-01 */
+    to: string
+  }
+}
+
+export type RequestExportPayments = {
+  /** Статус платежа */
+  status?: PaymentStatus
+  /** Дата создания платежей */
+  created_at?: {
+    /** @example 2022-11-01 */
+    from: string
+    /** @example 2022-11-01 */
+    to: string
+  }
+  /** Дата изменения статуса платежа */
+  status_changed_at?: {
+    /** @example 2022-11-01 */
+    from: string
+    /** @example 2022-11-01 */
+    to: string
+  }
+}
+
+export type RequestExportDeals = {
+  /** Статус заказа */
+  status?: DealStatus
+  /** Дата создания заказов */
+  created_at?: {
+    /** @example 2022-11-01 */
+    from: string
+    /** @example 2022-11-01 */
+    to: string
+  }
+  /** Дата оплаты заказа */
+  payed_at?: {
+    /** @example 2022-11-01 */
+    from: string
+    /** @example 2022-11-01 */
+    to: string
+  }
+  /** Дата завершения заказа */
+  finished_at?: {
+    /** @example 2022-11-01 */
+    from: string
+    /** @example 2022-11-01 */
+    to: string
+  }
+  /** Дата изменения статуса заказа */
+  status_changed_at?: {
+    /** @example 2022-11-01 */
+    from: string
+    /** @example 2022-11-01 */
+    to: string
+  }
+}
+
+export type ExportInfo = {
+  /** ID задачи, по которой можно получить информацию об экспорте */
+  export_id: number
+}
+
+export type FieldInfo = {
+  /** ID поля */
+  id: string
+  /** Сущность, в которой находится поле */
+  context_type: string
+  /** Тип поля */
+  type: string
+  /** Название поля */
+  title: string
+  /** Обязательность заполнения поля */
+  required: string
+  /** Порядковый номер поля */
+  field_order_pos: string
+  /** Настройки поля */
+  params: string
+}
+
+/**
+ * Выгруженные данные
+ * Структура данных для каждого объекта соответствует тому, что выгружается в CSV с помощью интерфейса экспорта в соответствующих разделах
+ * */
+export type ExportedData = {
+  /** Оглавление таблицы экспорта */
+  fields: string[]
+  /** Содержание таблицы экспорта */
+  items: (string | number)[][]
+}
