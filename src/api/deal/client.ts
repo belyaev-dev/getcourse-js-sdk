0b1484397c052525974e2b06c8ccc8ed34289274
Denis Belyaev <battler73@yandex.ru>
2023-06-16 16:07:10 +0300

    feat(deal): Добавлены описания методов

--- src/api/deal/client.ts
+++ src/api/deal/client.ts
@@ -3,6 +3,11 @@ import type { ImportApiResponse } from '../../typings/utility'
 import type { RequestAddDeal, RequestUpdateDealStatus, ResponseDeal } from './types'
 
 export class DealApi extends Endpoint {
+  /**
+   * Создание заказа в GetCourse
+   * @param {RequestAddDeal} deal Информация о заказе и пользователе
+   * @returns {ImportApiResponse<ResponseDeal>} Созданный заказ
+   */
   addDeal(deal: RequestAddDeal): Promise<ImportApiResponse<ResponseDeal>> {
     return this.rest.post<ImportApiResponse<ResponseDeal>>({
       url: '/pl/api/deals',
@@ -11,6 +16,14 @@ export class DealApi extends Endpoint {
     })
   }
 
+  /**
+   * Изменить статус заказа
+   *
+   * Если заказ имеет статус «Завершён» и вы меняете статус при помощи API на любой другой, то активные покупки, связанные с заказом, не будут отменены.
+   * То есть без изменения статуса покупки у пользователя сохранится доступ, выданный этой покупкой
+   * @param {RequestUpdateDealStatus} deal Запрос на обновление статуса заказа
+   * @returns {ImportApiResponse<ResponseDeal>} Обновленный заказ
+   */
   updateStatus(deal: RequestUpdateDealStatus): Promise<ImportApiResponse<ResponseDeal>> {
     return this.rest.post<ImportApiResponse<ResponseDeal>>({
       url: '/pl/api/deals',
