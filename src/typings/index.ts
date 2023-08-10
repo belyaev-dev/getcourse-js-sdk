4044365870434571c151488af9dcf49effd8adf2
Denis Belyaev <battler73@yandex.ru>
2023-08-10 15:11:25 +0300

    fix: export new types from utility

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,3 +1,3 @@
 export type { System, User, Session, UserStatus, DealStatus, PaymentStatus, PaymentType, DealCurrency } from './entities'
 export type { Options, HttpMethod, GetCourseAction, RequestInit } from './lib'
-export type { JSONValue, RequireAtLeastOne, ExportApiResponse, ImportApiResponse } from './utility'
+export type { JSONValue, RequireAtLeastOne, ExportApiResponse, ImportApiResponse, ImportApiErrorResponse, ImportApiSuccessResponse } from './utility'
